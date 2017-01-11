784b52be93b2f06e5d00ea711e139110530af3c5
Yury Shkliaryk <shkliaryk.yu.n@gmail.com>
2017-01-11 03:32:03 +0300

    Working test provided

--- simulation.py
+++ simulation.py
@@ -5,19 +5,22 @@ import matplotlib.animation as animation
 
 N = 120
 M = 1
-r = 25e+4
-dt = 0.001
+r = 15e+4
+dt = 0.0002
+boundary = 300
+T=10
+initial_speed = (1000, 1000)
 
 
 def calculate_force(X, s):
     F = [[0, 0] for i in xrange(N)]
     for i in xrange(N):
-        F[i][1] += M*2
+        F[i][1] += M*1000
         for j in xrange(N):
             distance = calculate_distance(X[i], X[j])
-            friction_force = calculate_friction_force(distance, (6, 10))
+            friction_force = calculate_friction_force(distance, (4, 10))
             F[i][0] += (friction_force-r*(distance-s[i][j]))*(X[i][0]-X[j][0])/(distance+0.001)
-            F[i][1] += (friction_force-r*(distance-s[i][j]))*(X[i][1]-X[j][1])/(distance+0.001)#+M*2
+            F[i][1] += (friction_force-r*(distance-s[i][j]))*(X[i][1]-X[j][1])/(distance+0.001)
             s[i][j] = distance
     return F, s
 
@@ -35,14 +38,15 @@ def calculate_friction_force(distance, thresholds):
 def calculate_coordinates(x, v, a):
     for i in xrange(N):
         for coord in xrange(2):
-            x[i][coord] += v[i][coord]+a[i][coord]*dt
+            x[i][coord] += v[i][coord]*dt
     return x
 
 
 def calculate_speed(x, v, a):
     for i in xrange(N):
-        if x[i][1] > 400:
+        if x[i][1] >= boundary:
             v[i] = [0, 0]
+            x[i][1] = boundary-1
         else:
             for coord in xrange(2):
                 v[i][coord] += a[i][coord]*dt
@@ -56,43 +60,48 @@ def calculate_acceleration(F):
 
 def initialize_conditions():
     d = 6
+    '''
+    x = [[6*(i%12)+5, 6*(i/12)+5] for i in xrange(N)]
+    '''
     x = [range(2) for i in xrange(N)]
-    for i in xrange(20):
-        for j in xrange(d):
+    for j in xrange(20):
+        for i in xrange(d):
             x[i+d*(j-1)][0] = 8*i if j%2==0 else 8*i+4
             x[i+d*(j-1)][1] = 7*j+140
+
     s = [[calculate_distance(x[i], x[j]) for j in xrange(N)] for i in xrange(N)]
-    v = [[100, 60] for i in xrange(N)]
-    return x, v, s
+    v = [[initial_speed[0], initial_speed[1]] for i in xrange(N)]
+    a = [[0, 0] for i in xrange(N)]
+    return x, v, a, s
 
 
 def calculate_distance(point1, point2):
     return sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
 
 
-def simulate_frame(x, v, s):
+def simulate_frame(x, v, a, s):
+    x = calculate_coordinates(x, v, a)
+    print x[::10]
     F, s = calculate_force(x, s)
     a = calculate_acceleration(F)
     v = calculate_speed(x, v, a)
-    y = calculate_coordinates(x, v, a)
-    return y, v, s
+    return x, v, a, s
 
 
 def simulate(number_of_frames):
-    x, v, s = initialize_conditions()
+    x, v, a, s = initialize_conditions()
     X = [copy.deepcopy(x)]
     for i in xrange(number_of_frames):
-        x, v, s = simulate_frame(x, v, s)
+        x, v, a, s = simulate_frame(x, v, a, s)
         X.append(copy.deepcopy(x))
-    print X[0]
-    print X[1]
     return X
 
 
 def make_animation(X):
     fig = plt.figure()
     ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
-                     xlim=(0, 1000), ylim=(0, 1000))
+                     xlim=(0, 500), ylim=(100, boundary*1.1))
+    plt.plot([0, 500], [boundary, boundary], 'k-')
     particles, = ax.plot([], [], 'bo', ms=5)
 
     def init():
@@ -107,12 +116,16 @@ def make_animation(X):
         particles.set_markersize(ms)
         return particles,
 
-    ani = animation.FuncAnimation(fig, animate, frames=15,
-                              interval=100, blit=True, init_func=init)
+    ani = animation.FuncAnimation(fig, animate, frames=T,
+                              interval=1, blit=True, init_func=init)
+
+    #Writer = animation.writers[u'imagemagick']
+    #writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+    #ani.save('mymovie.mp4',writer=writer)
+    #ani.save('ani.gif', writer='imagemagick'.encode('ascii'), fps=30)
     plt.show()
 
 
 if __name__ == '__main__':
-    X = simulate(15)
+    X = simulate(T)
     make_animation(X)
-    print "done"

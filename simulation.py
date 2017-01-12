c9f7b24e3019240271b33632554a671c9750b4ed
Yury Shkliaryk <shkliaryk.yu.n@gmail.com>
2017-01-13 00:35:10 +0300

    Include numpy

--- simulation.py
+++ simulation.py
@@ -1,26 +1,28 @@
 import visualization
-from math import *
-import copy
+import numpy as np
+import scipy.spatial.distance as dist
 
 N = 120
 M = 1
 r = 15e+4
 dt = 0.0002
 boundary = 300
-initial_speed = (1000, 1000)
+initial_speed = np.array([1000, 1000])
 
 
 def calculate_force(X, s):
-    F = [[0, 0] for i in xrange(N)]
+    F = np.zeros((N, 2))
+    actual_s = dist.squareform(dist.pdist(X))
     for i in xrange(N):
-        F[i][1] += M*1000
+        F[i, 1] += M*1000
         for j in xrange(N):
-            distance = calculate_distance(X[i], X[j])
-            friction_force = calculate_friction_force(distance, (4, 10))
-            F[i][0] += (friction_force-r*(distance-s[i][j]))*(X[i][0]-X[j][0])/(distance+0.001)
-            F[i][1] += (friction_force-r*(distance-s[i][j]))*(X[i][1]-X[j][1])/(distance+0.001)
-            s[i][j] = distance
-    return F, s
+            friction_force = calculate_friction_force(actual_s[i, j], (4, 10))
+            distance = actual_s[i, j]
+            distance_modification = actual_s[i, j] - s[i, j]
+            shift = X[i]-X[j]
+            F[i] += (friction_force-r*distance_modification)*shift/(distance+0.001)
+    s = actual_s
+    return F
 
 
 def calculate_friction_force(distance, thresholds):
@@ -33,27 +35,21 @@ def calculate_friction_force(distance, thresholds):
         return 0
 
 
-def calculate_coordinates(x, v, a):
-    for i in xrange(N):
-        for coord in xrange(2):
-            x[i][coord] += v[i][coord]*dt
-    return x
+def calculate_coordinates(x, v):
+    x += v*dt
 
 
 def calculate_speed(x, v, a):
     for i in xrange(N):
-        if x[i][1] >= boundary:
+        if x[i, 1] >= boundary:
             v[i] = [0, 0]
-            x[i][1] = boundary-1
+            x[i, 1] = boundary-1
         else:
-            for coord in xrange(2):
-                v[i][coord] += a[i][coord]*dt
-    return v
+            v[i] += a[i]*dt
 
 
 def calculate_acceleration(F):
-    a = [[F[i][0]/M, F[i][1]/M] for i in xrange(N)]
-    return a
+    return F / M
 
 
 def initialize_conditions():
@@ -61,41 +57,37 @@ def initialize_conditions():
     '''
     x = [[6*(i%12)+5, 6*(i/12)+5] for i in xrange(N)]
     '''
-    x = [range(2) for i in xrange(N)]
+    x = np.zeros((N, 2))
     for j in xrange(20):
         for i in xrange(d):
-            x[i+d*(j-1)][0] = 8*i if j%2==0 else 8*i+4
-            x[i+d*(j-1)][1] = 7*j+140
+            x[i+d*(j-1), 0] = 8*i if j%2==0 else 8*i+4
+            x[i+d*(j-1), 1] = 7*j+140
 
-    s = [[calculate_distance(x[i], x[j]) for j in xrange(N)] for i in xrange(N)]
-    v = [[initial_speed[0], initial_speed[1]] for i in xrange(N)]
-    a = [[0, 0] for i in xrange(N)]
+    s = dist.squareform(dist.pdist(x))
+    v = np.fromfunction(lambda i, j: initial_speed[j], (N, 2), dtype=int)
+    a = np.zeros((N, 2))
     return x, v, a, s
 
 
-def calculate_distance(point1, point2):
-    return sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
-
-
 def simulate_frame(x, v, a, s):
-    x = calculate_coordinates(x, v, a)
-    F, s = calculate_force(x, s)
+    calculate_coordinates(x, v)
+    F = calculate_force(x, s)
     a = calculate_acceleration(F)
-    v = calculate_speed(x, v, a)
+    calculate_speed(x, v, a)
     return x, v, a, s
 
 
 def simulate(number_of_frames):
     x, v, a, s = initialize_conditions()
-    X = [copy.deepcopy(x)]
+    X = [np.copy(x)]
     for i in xrange(number_of_frames):
-        x, v, a, s = simulate_frame(x, v, a, s)
-        X.append(copy.deepcopy(x))
+        simulate_frame(x, v, a, s)
+        X.append(np.copy(x))
         print 'Time frame number:', i
     return X
 
 
 if __name__ == '__main__':
-    time_frames = 500
+    time_frames = 3000
     X = simulate(time_frames)
     visualization.make_animation(X, time_frames, (0, 500), (100, boundary*1.1), 1, boundary)
